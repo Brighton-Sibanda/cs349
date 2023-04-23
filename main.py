b9114942b854e289de99b7bff122db50757f1356
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2023-04-23 16:10:52 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#from thefuzz import fuzz, process
+#import numpy as np
+import pandas as pd
+#import tensorflow as tf
+#import csv
+#import json
+#from tensorflow import keras
+
+
+review_path = "devided_dataset_v2/CDs_and_Vinyl/train/review_training.json"
+product_path = "devided_dataset_v2/CDs_and_Vinyl/train/product_training.json"
+product_data = pd.read_json(product_path)
+review_data = pd.read_json(review_path)
+
+#Training and coming up with feature vector
+feature_vector = {}  # key = product ; values = array of final feature values
+temp_dict = {} # temp dict to compile current feature info for a product accross features
+
+productIDs = product_data[['asin', 'awesomeness']]
+length = len(list(productIDs['asin']))
+for i in range(length):
+    feature_vector[productIDs['asin'][i]] = [productIDs['awesomeness'][i]]
+    temp_dict[productIDs['asin'][i]] = [productIDs['awesomeness'][i],[], [], [],[],[],[]]
+    
+#order array indices according to order numbered here; for reature vector
+
+#1 avg positive summary 
+#2 avg negative summary
+#3 avg positive review text
+#4 avg negative review text
+#5 avg vote credibility score
+#6 image credibility score
+#7 verified credibility score
+#8 time score
+
+#data collection
+review_sizes = len(list(review_data['asin']))
+
+for i in range(review_sizes):
+    # list of relevant_data = [awesomeness, [summaries],[reviewTexts], [votes], [time], [verified], [images]
+    # summaries = temp_dict[review_data['asin'][i]][1]
+    summary_text = lemmatize(review_data['summaries'][i])
+    
+    # but # lemmatize and remove stop words first 
+    
+    new_addition = get_sentiment(summary_text)
+    temp_dict[review_data['asin'][i]] = temp_dict[review_data['asin'][i]]
+        
+   
+    
+def lemmatize(mystr):
+    return mystr
+
+def get_sentiment(my_str):
+    
+    return 0
+
+
+
