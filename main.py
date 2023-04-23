a8ed1a24feeb4b59881bdde79e6efffe974661ad
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2023-04-23 17:55:22 -0500

    initial commit

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ productIDs = product_data[['asin', 'awesomeness']]
 length = len(list(productIDs['asin']))
 for i in range(length):
     feature_vector[productIDs['asin'][i]] = [productIDs['awesomeness'][i]]
-    temp_dict[productIDs['asin'][i]] = [productIDs['awesomeness'][i],[], [], [],[],[],[],[]]
+    temp_dict[productIDs['asin'][i]] = [[], [], [],[],[],[],[]]
     
 #order array indices according to order numbered here; for reature vector
 
@@ -48,57 +48,52 @@ def get_sentiment(my_str):
 
 # now collecting relevant information and making conversions for all reviews
 for i in range(review_sizes):
-    # list of relevant_data = [awesomeness, [summ scores],[text scores], [votes], [times], [verifieds], [images]]
+    # list of relevant_data = [[summ scores],[text scores], [votes], [times], [verifieds], [images]]
     
     # for summary
-    summary_text = lemmatize(review_data['summaries'][i])
+    summary_text = lemmatize(review_data['summary'][i])
     new_addition = get_sentiment(summary_text)
     c = temp_dict[review_data['asin'][i]]
-    final_summary = [c[0], c[1] + [new_addition], c[2], c[3],c[4],c[5],c[6],c[7]]
+    final_summary = [c[0] + [new_addition], c[1], c[2],c[3],c[4],c[5],c[6]]
     temp_dict[review_data['asin'][i]] =  final_summary
     
     #for reviewtext
     review_text = lemmatize(review_data['reviewText'][i])
     new_addition = get_sentiment(review_text)
     c = temp_dict[review_data['asin'][i]]
-    final_review = [c[0], c[1], c[2]+ [new_addition], c[3],c[4],c[5],c[6],c[7]]
+    final_review = [c[0], c[1]+ [new_addition], c[2],c[3],c[5],c[5],c[6]]
     temp_dict[review_data['asin'][i]] =  final_review
     
     # for vote score
     vote = review_data['vote'][i]
     c = temp_dict[review_data['asin'][i]]
-    final_votes = [c[0], c[1], c[2], c[3]+ [vote],c[4],c[5],c[6],c[7]]
+    final_votes = [c[0], c[1], c[2]+ [vote],c[3],c[4],c[5],c[6]]
     temp_dict[review_data['asin'][i]] =  final_votes
 
     # for time
     time = review_data['ReviewTime'][i]
     c = temp_dict[review_data['asin'][i]]
-    final_time = [c[0], c[1], c[2], c[3], c[4]+[time],c[5],c[6],c[7]]
+    final_time = [c[0], c[1], c[2], c[3]+[time],c[4],c[5],c[6]]
     temp_dict[review_data['asin'][i]] =  final_time
 
     # for image
     image = review_data['image'][i]
     if image == 'none':
-        image =0
+        image = 0
     else:
         image = 1
     c = temp_dict[review_data['asin'][i]]
-    final_time = [c[0], c[1], c[2], c[3], c[4],c[5]+[image],c[6],c[7]]
-    temp_dict[review_data['asin'][i]] =  final_time
+    final_image = [c[0], c[1], c[2], c[3], c[4]+[image],c[5],c[6]]
+    temp_dict[review_data['asin'][i]] =  final_image
 
     # for verified
     verified = review_data['verified'][i]
-    if verified == 0:
-        verified =0
-    else:
-        verified = 1
     c = temp_dict[review_data['asin'][i]]
-    final_time = [c[0], c[1], c[2], c[3], c[4],c[5]+[verified],c[6],c[7]]
-    temp_dict[review_data['asin'][i]] =  final_time
+    final_verified = [c[0], c[1], c[2], c[3], c[4]+[verified],c[5],c[6]]
+    temp_dict[review_data['asin'][i]] =  final_verified
 
 
 # now to calculate averages with weights accounted for
-
 
   
     
