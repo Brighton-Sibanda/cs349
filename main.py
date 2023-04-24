a476150cb51a6acbbb50fa197cc1b2a7799889c2
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2023-04-23 22:23:03 -0500

    functions

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import csv
 import json
-from textblob import TextBlob
-from textblob import Word
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 nltk.download('vader_lexicon')
@@ -17,18 +15,7 @@ review_data = pd.read_json(review_path)
 
 '''
 #Training and coming up with feature vector
-feature_vector = {}  # key = product ; values = array of final feature values
-temp_dict = {} # temp dict to compile current feature info for a product accross features
-
-productIDs = product_data
-length = len(list(productIDs['asin']))
-for i in range(length):
-    feature_vector[productIDs['asin'][i]] = [productIDs['awesomeness'][i]]
-    temp_dict[productIDs['asin'][i]] = [[], [], [],[],[],[],[]]
-    
-#order array indices according to order numbered here; for reature vector
-
-#1 avg positive summary 
+avg positive summary 
 #2 avg negative summary
 #3 avg positive review text
 #4 avg negative review text
@@ -37,59 +24,7 @@ for i in range(length):
 #7 verified credibility score
 #8 time score
 
-#data collection functions
-review_sizes = len(list(review_data['asin']))
-def lemmatize(mystr):
-    stop_words = set(nltk.corpus.stopwords.words('english'))
-    words = TextBlob(mystr).words
-    words = [word.lemmatize() for word in words if word.lower() not in stop_words]
-    lemmatized_text = " ".join(words)
-    return lemmatized_text
-
-# now collecting relevant information and making conversions for all reviews
-for i in range(review_sizes):
-    # list of relevant_data = [[summ scores],[text scores], [votes], [times], [verifieds], [images]]
-    
-    # for summary
-    
-    
-    #for reviewtext
-    review_text = lemmatize(review_data['reviewText'][i])
-    new_addition = get_sentiment(review_text)
-    c = temp_dict[review_data['asin'][i]]
-    final_review = [c[0], c[1]+ [new_addition], c[2],c[3],c[5],c[5],c[6]]
-    temp_dict[review_data['asin'][i]] =  final_review
-    
-    # for vote score
-    vote = review_data['vote'][i]
-    c = temp_dict[review_data['asin'][i]]
-    final_votes = [c[0], c[1], c[2]+ [vote],c[3],c[4],c[5],c[6]]
-    temp_dict[review_data['asin'][i]] =  final_votes
-
-    # for time
-    time = review_data['ReviewTime'][i]
-    c = temp_dict[review_data['asin'][i]]
-    final_time = [c[0], c[1], c[2], c[3]+[time],c[4],c[5],c[6]]
-    temp_dict[review_data['asin'][i]] =  final_time
-
-    # for image
-    image = review_data['image'][i]
-    if image == 'none':
-        image = 0
-    else:
-        image = 1
-    c = temp_dict[review_data['asin'][i]]
-    final_image = [c[0], c[1], c[2], c[3], c[4]+[image],c[5],c[6]]
-    temp_dict[review_data['asin'][i]] =  final_image
-
-    # for verified
-    verified = review_data['verified'][i]
-    c = temp_dict[review_data['asin'][i]]
-    final_verified = [c[0], c[1], c[2], c[3], c[4]+[verified],c[5],c[6]]
-    temp_dict[review_data['asin'][i]] =  final_verified
-
-
-    # now to calculate averages with weights accounted for'''
+'''
 def get_sentiment(text):
     analyzer = SentimentIntensityAnalyzer()
     scores = analyzer.polarity_scores(text)
@@ -132,14 +67,34 @@ def get_num_votes(df):
 on the relative time it was sent
 """
 
-""" function to substitute 'None' for False and True otherwise for 
- the images column
-"""
 
-""" function to count number of positive reviews with images and 
-negative reviews with images"""
+def replace_none_with_zero(df):
+    """ function to substitute 'None' for False and True otherwise for 
+     the images column
+    """
+    df['image'] = df['image'].apply(lambda x: False if x == 'None' else True)
+    return df
 
-"""function to count number of positive verified and negative verified"""
+
+def image_review_count(df):
+    """ function to count number of positive reviews with images and 
+    negative reviews with images"""
+    
+    df = df[df["image"]]
+    pos = len(df[df["positive"]])
+    neg = len(df[df["positive"]==False])
+    return (pos, neg)
+
+def num_verified(df):
+
+    """function to count number of positive verified and negative verified"""
+
+    df = df[df["verified"]]
+    pos = len(df[df["positive"]])
+    neg = len(df[df["positive"]==False])
+    return (pos, neg)
 
 """loop through the products and construct the row with the feature vector 
 values"""
+
+
