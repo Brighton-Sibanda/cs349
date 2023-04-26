7b891b988dbfe0a9d9ec76f6a1074ca4eabfbf00
Rafa <rafaelcoelho2024@u.northwestern.edu>
2023-04-25 20:38:38 -0500

    new

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 # Reading training data from CSV file
 feature_vector_path = "feature_vector.csv"
@@ -19,6 +20,14 @@ nb_model.fit(train_ind, train_dep)
 
 # Predicting the classification of training data
 y_pred = nb_model.predict(train_ind)
+
+# Calculating precision, recall, and F1 score of the model on test data
+precision = precision_score(train_dep, y_pred, average='macro')
+recall = recall_score(train_dep, y_pred, average='macro')
+f1 = f1_score(train_dep, y_pred, average='macro')
+print("Precision: {:.2f}".format(precision))
+print("Recall: {:.2f}".format(recall))
+print("F1 Score: {:.2f}".format(f1))
 
 # Calculating the accuracy of the model on training data
 accuracy_train = accuracy_score(train_dep, y_pred)
