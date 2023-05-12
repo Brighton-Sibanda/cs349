f39d74ba731a192130046f49bc52f061925c1f08
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2023-05-12 00:52:15 -0500

    final model start

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-import pandas as pd
-import csv
-import json
+#import csv
+#import json
 import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#from nltk.sentiment.vader import SentimentIntensityAnalyzer
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
-from nltk.stem import WordNetLemmatizer
+#from nltk.stem import WordNetLemmatizer
 nltk.download('wordnet')
-from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 import warnings
+import pandas as pd
+#from nltk.corpus import stopwords
+#from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import GridSearchCV
 
-warnings.filterwarnings("ignore")
 
-
-
-# Collecting all files and cleaning up None values
+# Collecting all training files and cleaning up None values
 review_path = "devided_dataset_v2/CDs_and_Vinyl/train/review_training.json"
 product_path = "devided_dataset_v2/CDs_and_Vinyl/train/product_training.json"
 product_data = pd.read_json(product_path)
@@ -27,7 +26,7 @@ review_data["reviewText"] = review_data["reviewText"].fillna("negative")
 review_data['vote'] = review_data['vote'].apply(lambda x: 0 if x == None else int(x.replace(',','')))
 review_data['image'] = review_data['image'].apply(lambda x: False if x == None else True)
 
-
+# Collecting all testing files and cleaning up None values
 test_review_path = "devided_dataset_v2/CDs_and_Vinyl/test1/review_test.json"
 test_product_path = "devided_dataset_v2/CDs_and_Vinyl/test1/product_test.json"
 test_product_data = pd.read_json(test_product_path)
@@ -37,88 +36,84 @@ test_review_data["reviewText"] = test_review_data["reviewText"].fillna("negative
 test_review_data['vote'] = test_review_data['vote'].apply(lambda x: 0 if x == None else int(x.replace(',','')))
 test_review_data['image'] = test_review_data['image'].apply(lambda x: False if x == None else True)
 
+# Initializing vectorizer and corpus for the TF-IDF score calculations
+id_vector_awesome = product_data[product_data["awesomeness"] == 1]['asin']
+df_awesome_corpus = review_data.loc[review_data['asin'].isin(id_vector_awesome), ['asin', 'summary']].reset_index(drop=True)
+awesome_corpus = list(df_awesome_corpus.loc[:, 'summary']) # awesome corpus
+
+id_vector_notawesome = product_data[product_data["awesomeness"] == 0]['asin']
+df_notawesome_corpus = review_data.loc[review_data['asin'].isin(id_vector_notawesome), ['asin', 'summary']].reset_index(drop=True)
+notawesome_corpus = list(df_notawesome_corpus.loc[:, 'summary']) # not awesome corpus
+
+vectorizer_awesome = TfidfVectorizer(stop_words='english') # Awesome CountVectorizer object
+vectorizer_notawesome = TfidfVectorizer(stop_words='english') # Not Awesome CountVectorizer object
+
+train_tfidf_awesome = vectorizer_awesome.fit_transform(awesome_corpus) # Fit the vectorizer to the corpus for awesome
+train_dtfidf_notawesome = vectorizer_notawesome.fit_transform(notawesome_corpus) # Fit the vectorizer to the corpus for not awesome
+
+vocabulary_awesome = vectorizer_awesome.get_feature_names() # Awesome vocabulary
+vocabulary_notawesome = vectorizer_notawesome.get_feature_names() # Not awesome vocabulary
 
 # slice the data to retain only the first 1000 rows
-product_data = product_data.iloc[:10000, :]
+product_data = product_data.iloc[:1000, :]
+
+warnings.filterwarnings("ignore")
 
 
-'''
- Our feature vector  is as follows 
-
-#1 num_pos ->  ratio of positive reviews and summaries (based on sentiments)
-#2 num_neg -> ratio of negative reviews and summaries
-#3 vote_score -> weighting positive votes vs negatuve votes
-#4 pos_image_count -> counting which positive reviews have images
-#5 neg_image_count -> counting which negative reviews have images
-#6 pos_verified_count -> counting which positive reviews are verified
-#7 neg_verified_count -> counting which negative reviews are verified
-#8 pos_time_score
-#9 neg_time_score
-
-'''
-awesome_corpus = ""
-non_awesome_corpus = ""
-def get_TFIDF(text, choices):
+def get_TFIDF(text):
     
-    result = process.extractBests(text, choices, score_cutoff=0, limit=None, scorer = fuzz.token_sort_ratio) 
-    return [result[0][1], result[1][1]]
-    
+    # Transform the testing reviews into TF-IDF scores using the same vectorizer
+    test_tfidf_awesome = vectorizer_awesome.transform(text)
+    test_tfidf_not_awesome = vectorizer_notawesome.transform(text)
+
+    # Transforming document-term matrix to a dataframe
+    test_tfidf_awesome_df = pd.DataFrame(test_tfidf_awesome.toarray(), columns=vocabulary_awesome)
+    test_tfidf_not_awesome_df = pd.DataFrame(test_tfidf_not_awesome.toarray(), columns=vocabulary_notawesome)
+
+    # Sum the columns together and create a new column with the results
+    aggregate_tfidf_awesome = test_tfidf_awesome_df.sum(axis=1)
+    aggregate_tfidf_not_awesome = test_tfidf_not_awesome_df.sum(axis=1)
+
+    return [aggregate_tfidf_awesome, aggregate_tfidf_not_awesome]
 
 
-
-global analyzer
-analyzer = SentimentIntensityAnalyzer()
-def get_sentiment(text):
-    ''' function to get the sentiment of a text'''
-    global analyzer
-    scores = analyzer.polarity_scores(text)
-    return scores['compound']
-    
-def add_sentiment_col(df): 
-    '''adds a columnn to dataframe; indicating positive or negative sentiment'''
-    
-    positive = []
-
-    for index, row in df.iterrows():
-        summary = row["summary"]
-        text = row["reviewText"]
-        summary_score = get_sentiment(summary)
-        text_score = get_sentiment(text)
-        if summary_score == 0:
-            sentiment_score = 2*text_score
-        elif text_score == 0:
-            sentiment_score = 2 * summary_score
-        else:
-            sentiment_score = summary_score + text_score
-
-        if sentiment_score>0.6:
-            positive.append(True)
-        else:
-            positive.append(False)
-    df['positive'] = positive
-    return df
+def is_positive(df, text1, text2):
+    return df[text1].mean() > df[text2].mean()
 
 
-def get_num_pos_neg(df):
-    """ 
-    input: pandas dataframe with all reviews for one product
-    output: two-element tuple with first element being the 
-            number of positive reviews and the second element 
-            being the number of negative reviews
-    """
-    pos = len(df[df["positive"] == True])
-    neg = len(df[df["positive"] == False])
-    return (pos, neg)
+def is_negative(df, text1, text2):
+    return df[text1].mean() < df[text2].mean()
+
 
 def get_vote_score(df):
-
-
-    pos = df[df["positive"] == True]
-    pos = pos['vote'].sum()
-    neg = df[df["positive"]==False]
-    neg = neg['vote'].sum()
+    
+    pos = 0
+    neg = 0 
+    pos_list1, neg_list1  = list(df['aw_rt']), list(df['naw_rt'])
+    pos_list2, neg_list2  = list(df['aw_s']), list(df['naw_s'])
+    vote_list = list(df['vote'])
+    i = 0
+    
+    while i < len(pos_list1):
+        
+        temp_pos = pos
+        temp_neg = neg
+        
+        if pos_list1[i] > neg_list1[i]:
+            pos += vote_list[i]
+        elif pos_list1[i] < neg_list1[i]:
+            neg += vote_list[i]
+            
+        if (pos_list2[i] > neg_list2[i]) and temp_pos == pos:
+            pos += vote_list[i]
+        elif pos_list1[i] < neg_list1[i] and temp_neg == neg:
+            neg += vote_list[i]
+        
+        i += 1
+    
 
     return (pos + 1)/(neg + 1)
+
 
 def calculate_time_score(df):
     """function the gives a value between 0 and 1 for a review depending
@@ -146,90 +141,95 @@ def calculate_time_score(df):
         neg_scores.append(0.5)
     return (sum(pos_scores)/len(pos_scores), sum(neg_scores)/len(neg_scores))
 
+
+
 def image_review_count(df):
     """ function to count number of positive reviews with images and 
     negative reviews with images"""
     
-    df_new = df[df["image"]]
-    pos = len(df_new[df_new["positive"] == True])
-    neg = len(df_new[df_new["positive"]==False])
-    return (pos, neg)
+    df_new = df[df["image"]==True]
+    pos1 = is_positive(df_new, 'aw_rt', 'naw_rt')
+    neg1 = is_negative(df_new, 'aw_rt', 'naw_rt')
+     
+    
+    pos2 = is_positive(df_new, 'aw_s', 'naw_s')
+    neg2 = is_negative(df_new, 'aw_s', 'naw_s')
+    
+    
+    return (pos1 + pos2) - (neg1 + neg2)
 
 def num_verified(df):
 
     """function to count number of positive verified and negative verified"""
 
     df_new = df[df["verified"]==True]
-    pos = len(df_new[df_new["positive"]==True])
-    neg = len(df_new[df_new["positive"]==False])
-    return (pos, neg)
+    pos1 = is_positive(df_new, 'aw_rt', 'naw_rt')
+    neg1 = is_negative(df_new, 'aw_rt', 'naw_rt')
+     
+    
+    pos2 = is_positive(df_new, 'aw_s', 'naw_s')
+    neg2 = is_negative(df_new, 'aw_s', 'naw_s')
+    
+    
+    return (pos1 + pos2) - (neg1 + neg2)
 
-
-feature_vector = pd.DataFrame({"num_pos":[], "num_neg":[], "vote_score":[], "pos_image_count":[], "neg_image_count":[], "pos_verified_count":[], "neg_verified_count":[], "pos_time_score":[], "neg_time_score":[]})
+    
+feature_vector = pd.DataFrame({"aw_rt":[], "naw_rt":[], "aw_s":[], "naw_s":[],"vote_score":[], "image_score":[], "verified":[]})
 iDs = list(product_data['asin'])
-
-# create a WordNetLemmatizer object
-lemmatizer = WordNetLemmatizer()
-
-# define a function to lemmatize and remove stopwords from a sentence
-def lemmatize_sentence(sentence, filtered_words):
-    # tokenize the sentence into words
-    words = nltk.word_tokenize(sentence.lower())
-
-    # remove stopwords from the list of words
-    stopwords_list = stopwords.words('english')
-    filtered_words = [word for word in words if word not in stopwords_list]
-    
-    # lemmatize each word in the filtered list of words
-    lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_words]
-    
-    # join the lemmatized words back into a sentence
-    return ' '.join(lemmatized_words)
-
-def get_all_words(df, col_name, id_list):
-    #filter data
-    df = df[df['asin'].isin(id_list)]
-    #concatenate
-    concatenated_text = df[col_name].str.cat(sep=' ')
-
-    return lemmatize_sentence(concatenated_text)
 
 def make_feature_vector(iDs, feature_vector, review_data):
     
     """loop through the products and construct the row with the feature vector 
     values"""
+    
     k = 0
     for i in iDs:
         if (k % 1000 == 0):
             print(k)
         k += 1
         current_data = review_data[review_data["asin"] == i]
-        text_sentiments = add_sentiment_col(current_data)
-        num_pos, num_neg = get_num_pos_neg(text_sentiments)
-        pos_ratio = (num_pos)/(num_neg + num_pos)
-        neg_ratio = (num_neg)/(num_pos + num_neg)
-        vote_score = get_vote_score(text_sentiments)
-        pos_image_count, neg_image_count = image_review_count(text_sentiments)
-        pos_verified_count, neg_verified_count = num_verified(text_sentiments)
-        pos_time_score, neg_time_score  = calculate_time_score(text_sentiments)
-        feature_vector.loc[len(feature_vector)] = [pos_ratio, neg_ratio, vote_score, pos_image_count, neg_image_count, pos_verified_count, neg_verified_count, pos_time_score, neg_time_score]
+        aw_rt, naw_rt  = get_TFIDF(list(current_data['reviewText']))
+        aw_s, naw_s  = get_TFIDF(list(current_data['summary']))
+        
+        current_data['aw_rt'] = list(aw_rt)
+        current_data['naw_rt'] = list(naw_rt)
+        current_data['aw_s'] = list(aw_s)
+        current_data['naw_s'] = list(naw_s)
+        
+        vote_score = get_vote_score(current_data)
+        image_score = image_review_count(current_data)
+        verified = num_verified(current_data)
+        
+        aw_rt1 = current_data['aw_rt'].mean()
+        naw_rt1 = current_data['naw_rt'].mean()
+        aw_s1 = current_data['aw_s'].mean()
+        naw_s1 = current_data['naw_s'].mean()
+        
+        feature_vector.loc[len(feature_vector)] = [aw_rt1, naw_rt1, aw_s1, naw_s1, vote_score, image_score, verified]
     return feature_vector
 
 train_feature_vector = make_feature_vector(iDs, feature_vector, review_data)
 train_feature_vector["awesomeness"] = list(product_data["awesomeness"])
 
+
+
+    
+
 #NOW TRAINING
 X = train_feature_vector.iloc[:, :-1]
 y = train_feature_vector.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+clf = GradientBoostingClassifier()
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+
 
 # Define the parameter grid
 param_grid = {
     'learning_rate': [0.1, 0.01],
     'n_estimators': [100, 200],
     'max_depth': [3, 5],
-    'min_samples_split': [2, 4]
-}
+    'min_samples_split': [2, 4] }
 
 # Create the model
 clf = GradientBoostingClassifier()
@@ -248,6 +248,8 @@ clf.fit(X_train, y_train)
 # Calculating Score
 score = clf.score(X_test, y_test)
 
+
+'''
 #Now for testing
 iDs = list(test_product_data['asin'])
 feature_vector_2 = pd.DataFrame({"num_pos":[], "num_neg":[], "vote_score":[], "pos_image_count":[], "neg_image_count":[], "pos_verified_count":[], "neg_verified_count":[], "pos_time_score":[], "neg_time_score":[]})
@@ -256,4 +258,14 @@ predicted_class = clf.predict(test_feature_vector)
 
 final_json = test_product_data
 final_json["awesomeness"] = predicted_class
-final_json.to_json("predictions.json")
+final_json.to_json("predictions.json")'''
+
+
+
+
+
+
+
+
+
+
