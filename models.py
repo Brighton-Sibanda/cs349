5c298a8b1c630ab01130e81e59e6b1c2c1de04c5
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2023-05-12 23:11:29 -0500

    final model

--- models.py
+++ models.py
@@ -9,6 +9,9 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
+import warnings
+warnings.filterwarnings("ignore")
+
 
 # Reading training data from CSV file
 feature_vector_path = "feature_vectorv2.csv"
@@ -84,66 +87,29 @@ lr_model = LogisticRegression()
 rf_model = RandomForestClassifier()
 gb_model = GradientBoostingClassifier()
 
+
+
+# Calculating precision, recall, and F1 score of each model on training data
 # Performing Hyperparameter Optimization MultinomialNB - perform grid search cross-validation
 nb_grid_search = GridSearchCV(nb_model, param_grid_nb, cv=5)
 nb_grid_search.fit(train_ind, train_dep)
 nb_model = MultinomialNB(**nb_grid_search.best_params_)
-
-# Performing Hyperparameter Optimization KNeighborsClassifier - perform grid search cross-validation
-knn_grid_search = GridSearchCV(knn_model, param_grid_knn, cv=5)
-knn_grid_search.fit(train_ind, train_dep)
-knn_model = KNeighborsClassifier(**knn_grid_search.best_params_)
-
-# Performing Hyperparameter Optimization DecisionTreeClassifier - perform grid search cross-validation
-dt_grid_search = GridSearchCV(dt_model, param_grid_dt, cv=5)
-dt_grid_search.fit(train_ind, train_dep)
-dt_model = DecisionTreeClassifier(**dt_grid_search.best_params_)
-
-# Performing Hyperparameter Optimization SVC - perform grid search cross-validation
-svm_grid_search = GridSearchCV(svm_model, param_grid_svm, cv=5)
-svm_grid_search.fit(train_ind, train_dep)
-svm_model = SVC(**svm_grid_search.best_params_)
-
-# Performing Hyperparameter Optimization LogisticRegression - perform grid search cross-validation
-lr_grid_search = GridSearchCV(lr_model, param_grid_lr, cv=5)
-lr_grid_search.fit(train_ind, train_dep)
-lr_model = LogisticRegression(**lr_grid_search.best_params_)
-
-# Performing Hyperparameter Optimization RandomForestClassifier - perform grid search cross-validation
-rf_grid_search = GridSearchCV(rf_model, param_grid_rf, cv=5)
-rf_grid_search.fit(train_ind, train_dep)
-rf_model = RandomForestClassifier(**rf_grid_search.best_params_)
-
-# Performing Hyperparameter Optimization GradientBoostingClassifier - perform grid search cross-validation
-gb_grid_search = GridSearchCV(gb_model, param_grid_gb, cv=5)
-gb_grid_search.fit(train_ind, train_dep)
-gb_model = GradientBoostingClassifier(**gb_grid_search.best_params_)
-
-# Training the models
 nb_model.fit(train_ind, train_dep)
-knn_model.fit(train_ind, train_dep)
-dt_model.fit(train_ind, train_dep)
-svm_model.fit(train_ind, train_dep)
-lr_model.fit(train_ind, train_dep)
-rf_model.fit(train_ind, train_dep)
-gb_model.fit(train_ind, train_dep)
-
-# Predicting the classification of training data for each model
 nb_y_pred = nb_model.predict(train_ind)
-knn_y_pred = knn_model.predict(train_ind)
-dt_y_pred = dt_model.predict(train_ind)
-svm_y_pred = svm_model.predict(train_ind)
-lr_y_pred = lr_model.predict(train_ind)
-rf_y_pred = rf_model.predict(train_ind)
-gb_y_pred = gb_model.predict(train_ind)
 
-# Calculating precision, recall, and F1 score of each model on training data
 nb_precision = precision_score(train_dep, nb_y_pred, average='macro')
 nb_recall = recall_score(train_dep, nb_y_pred, average='macro')
 nb_f1 = f1_score(train_dep, nb_y_pred, average='macro')
 print("Multinomial Naive Bayes Precision: {:.2f}".format(nb_precision))
 print("Multinomial Naive Bayes Recall: {:.2f}".format(nb_recall))
 print("Multinomial Naive Bayes F1 Score: {:.2f}".format(nb_f1))
+
+
+knn_grid_search = GridSearchCV(knn_model, param_grid_knn, cv=5)
+knn_grid_search.fit(train_ind, train_dep)
+knn_model = KNeighborsClassifier(**knn_grid_search.best_params_)
+knn_model.fit(train_ind, train_dep)
+knn_y_pred = knn_model.predict(train_ind)
 
 knn_precision = precision_score(train_dep, knn_y_pred, average='macro')
 knn_recall = recall_score(train_dep, knn_y_pred, average='macro')
@@ -152,7 +118,11 @@ print("K-Nearest Neighbor Precision: {:.2f}".format(knn_precision))
 print("K-Nearest Neighbor Recall: {:.2f}".format(knn_recall))
 print("K-Nearest Neighbor F1 Score: {:.2f}".format(knn_f1))
 
-
+dt_grid_search = GridSearchCV(dt_model, param_grid_dt, cv=5)
+dt_grid_search.fit(train_ind, train_dep)
+dt_model = DecisionTreeClassifier(**dt_grid_search.best_params_)
+dt_model.fit(train_ind, train_dep)
+dt_y_pred = dt_model.predict(train_ind)
 dt_precision = precision_score(train_dep, dt_y_pred, average='macro')
 dt_recall = recall_score(train_dep, dt_y_pred, average='macro')
 dt_f1 = f1_score(train_dep, dt_y_pred, average='macro')
@@ -160,19 +130,26 @@ print("Decision Tree Precision: {:.2f}".format(dt_precision))
 print("Decision Tree Recall: {:.2f}".format(dt_recall))
 print("Decision Tree F1 Score: {:.2f}".format(dt_f1))
 
-svm_precision = precision_score(train_dep, svm_y_pred, average='macro')
-svm_recall = recall_score(train_dep, svm_y_pred, average='macro')
-svm_f1 = f1_score(train_dep, svm_y_pred, average='macro')
-print("Support Vector Machine Precision: {:.2f}".format(svm_precision))
-print("Support Vector Machine Recall: {:.2f}".format(svm_recall))
-print("Support Vector Machine F1 Score: {:.2f}".format(svm_f1))
 
+
+lr_grid_search = GridSearchCV(lr_model, param_grid_lr, cv=5)
+lr_grid_search.fit(train_ind, train_dep)
+lr_model = LogisticRegression(**lr_grid_search.best_params_)
+lr_model.fit(train_ind, train_dep)
+lr_y_pred = lr_model.predict(train_ind)
 lr_precision = precision_score(train_dep, lr_y_pred, average='macro')
 lr_recall = recall_score(train_dep, lr_y_pred, average='macro')
 lr_f1 = f1_score(train_dep, lr_y_pred, average='macro')
 print("Logistic Regression Precision: {:.2f}".format(lr_precision))
 print("Logistic Regression Recall: {:.2f}".format(lr_recall))
 print("Logistic Regression F1 Score: {:.2f}".format(lr_f1))
+
+
+rf_grid_search = GridSearchCV(rf_model, param_grid_rf, cv=5)
+rf_grid_search.fit(train_ind, train_dep)
+rf_model = RandomForestClassifier(**rf_grid_search.best_params_)
+rf_model.fit(train_ind, train_dep)
+rf_y_pred = rf_model.predict(train_ind)
 
 rf_precision = precision_score(train_dep, rf_y_pred, average='macro')
 rf_recall = recall_score(train_dep, rf_y_pred, average='macro')
@@ -181,22 +158,18 @@ print("Random Forest Precision: {:.2f}".format(rf_precision))
 print("Random Forest Recall: {:.2f}".format(rf_recall))
 print("Random Forest F1 Score: {:.2f}".format(rf_f1))
 
+
+gb_grid_search = GridSearchCV(gb_model, param_grid_gb, cv=5)
+gb_grid_search.fit(train_ind, train_dep)
+gb_model = GradientBoostingClassifier(**gb_grid_search.best_params_)
+gb_model.fit(train_ind, train_dep)
+gb_y_pred = gb_model.predict(train_ind)
 gb_precision = precision_score(train_dep, gb_y_pred, average='macro')
 gb_recall = recall_score(train_dep, gb_y_pred, average='macro')
 gb_f1 = f1_score(train_dep, gb_y_pred, average='macro')
 print("Gradient Boosting Precision: {:.2f}".format(gb_precision))
 print("Gradient Boosting Recall: {:.2f}".format(gb_recall))
 print("Gradient Boosting F1 Score: {:.2f}".format(gb_f1))
-
-knn_scores = cross_val_score(knn_model, train_ind, train_dep, cv=10, scoring='f1_macro')
-print("k nearest neighbor f1 Score (10-fold cross-validation): {:.2f}".format(knn_scores.mean()))
-
-svm_scores = cross_val_score(svm_model, train_ind, train_dep, cv=10, scoring='f1_macro')
-print("Support Vector Machine F1 Score (10-fold cross-validation): {:.2f}".format(svm_scores.mean()))
-
-lr_scores = cross_val_score(lr_model, train_ind, train_dep, cv=10, scoring='f1_macro')
-print("Logistic Regression F1 Score (10-fold cross-validation): {:.2f}".format(lr_scores.mean()))
-
 
 gb_scores = cross_val_score(gb_model, train_ind, train_dep, cv=10, scoring='f1_macro')
 gb_2scores = cross_val_score(gb_model, train_ind, train_dep, cv=10, scoring='precision')
@@ -209,6 +182,16 @@ print("Gradient Boosting recall Score (10-fold cross-validation): {:.2f}".format
 
 
 
+knn_scores = cross_val_score(knn_model, train_ind, train_dep, cv=10, scoring='f1_macro')
+print("k nearest neighbor f1 Score (10-fold cross-validation): {:.2f}".format(knn_scores.mean()))
+
+
+lr_scores = cross_val_score(lr_model, train_ind, train_dep, cv=10, scoring='f1_macro')
+print("Logistic Regression F1 Score (10-fold cross-validation): {:.2f}".format(lr_scores.mean()))
+
+
+
+
 nb_scores = cross_val_score(nb_model, train_ind, train_dep, cv=10, scoring='f1_macro')
 print("Naive Bayes F1 Score (10-fold cross-validation): {:.2f}".format(nb_scores.mean()))
 
@@ -218,3 +201,18 @@ print("Decision Tree F1 Score (10-fold cross-validation): {:.2f}".format(dt_scor
 
 rf_scores = cross_val_score(rf_model, train_ind, train_dep, cv=10, scoring='f1_macro')
 print("Random Forest F1 Score (10-fold cross-validation): {:.2f}".format(rf_scores.mean()))
+
+svm_grid_search = GridSearchCV(svm_model, param_grid_svm, cv=5)
+svm_grid_search.fit(train_ind, train_dep)
+svm_model = SVC(**svm_grid_search.best_params_)
+svm_model.fit(train_ind, train_dep)
+svm_y_pred = svm_model.predict(train_ind)
+svm_precision = precision_score(train_dep, svm_y_pred, average='macro')
+svm_recall = recall_score(train_dep, svm_y_pred, average='macro')
+svm_f1 = f1_score(train_dep, svm_y_pred, average='macro')
+print("Support Vector Machine Precision: {:.2f}".format(svm_precision))
+print("Support Vector Machine Recall: {:.2f}".format(svm_recall))
+print("Support Vector Machine F1 Score: {:.2f}".format(svm_f1))
+
+svm_scores = cross_val_score(svm_model, train_ind, train_dep, cv=10, scoring='f1_macro')
+print("Support Vector Machine F1 Score (10-fold cross-validation): {:.2f}".format(svm_scores.mean()))
